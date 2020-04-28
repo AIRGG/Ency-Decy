e855ce18269703decb51e7328b1f2633a40ad648
DTAX-01 <yolandayudina@gmail.com>
2020-04-28 10:46:35 +0700

    add to json POST request

--- app.py
+++ app.py
@@ -28,16 +28,35 @@ def apinya():
 		if apa == "ency":
 			pw = request.form["password"]
 			msg = request.form["message"]
+			tojsn = request.form["tojson"]
 			ency = EncyDecy(pw).ency(msg).decode("UTF-8")
+			if tojsn == "1":
+				return {
+					"chipertext":ency,
+					"password":pw
+				}
 			return render_template("hasil.html", pw=pw, hasil=ency, apa=apa)
 
 		if apa == "decy":
 			pw = request.form["password"]
 			msg = request.form["message"]
+			tojsn = request.form["tojson"]
 			decy = EncyDecy(pw).decy(msg)
 			if(not decy):
+				if tojsn == "1":
+					return {
+						"error":"Invalid Password!!!",
+						"chipertext":msg,
+						"password":pw
+					}
 				return "PASSWORDMU SALAH COKK <a href='/decrypt'>Back</a>"
 			else:
+				if tojsn == "1":
+					return {
+						"chipertext":msg,
+						"password":pw,
+						"msg":decy.decode("UTF-8")
+					}
 				return render_template("hasil.html", chiper=msg, pw=pw, hasil=decy.decode("UTF-8"), apa=apa)
 
 	return "UPSS"
@@ -45,5 +64,5 @@ def apinya():
 if __name__ == "__main__":
 	# http_server = WSGIServer(('0.0.0.0', 3001), app)
 	# http_server.serve_forever()
-	app.run()
-	# app.run(port=8082, debug=True, host="localhost")
+	# app.run()
+	app.run(port=8082, debug=True, host="localhost")
--- cryptnya.py
+++ cryptnya.py
@@ -1,4 +1,4 @@
-import os
+import os, random
 import base64
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
@@ -9,20 +9,20 @@ from cryptography.fernet import InvalidToken
 class EncyDecy(object):
 	"""docstring for EncyDecy"""
 	def __init__(self, mypass):
-		# self.mypass = mypass
-		# mypass = self.mypass
-		password = mypass.encode()
+		password = bytes(mypass.encode())
 		salt = b'^%$#ASIN@SALTNYA%^*}<?!?^%!|'
+		# salt = b'\xb4-\xe8L8\xbe\xaa\xfc\x04\xac\xec\x1b\xb0\xd2\xa6\xc2\xf8xc0\x00\xc4\xc1^\x08<\xbb\xba=\xfb\xf7_\x7fy{n[\xc64p\xf8\x11\xfe\xf1\xd3\x07\xee5\xa9Vf\xd9\x95\xe8^TZ\x92N\x7f\x81"\xe1\x19'
 		kdf = PBKDF2HMAC(
 		    algorithm=hashes.SHA256(),
 		    length=32,
 		    salt=salt,
-		    iterations=100000,
+		    iterations=100,
 		    backend=default_backend()
 		)
 		key = base64.urlsafe_b64encode(kdf.derive(password))
 		f = Fernet(key)
 		self.fernet = f
+		self.tmpname = "rahasia"
 	
 	def ency(self, message):
 		return self.fernet.encrypt(message.encode())
@@ -32,7 +32,31 @@ class EncyDecy(object):
 		try:
 			return self.fernet.decrypt(encymsg.encode())
 		except Exception as e:
+			print(e, "apa")
 			return False
 		# except InvalidToken as e:
 		# 	return False
 		pass
+
+	def encyFile(self, pathToFile):
+		try:
+			with open(pathToFile, 'rb') as f:
+				data = f.read()
+			encrypted = self.fernet.encrypt(data)
+			out = "{}.{}".format(os.path.basename(pathToFile), self.tmpname)
+			with open(out, 'wb') as f:
+				f.write(encrypted)
+		except Exception as e:
+			return False
+		pass
+
+	def decyFile(self, pathToFile):
+		try:
+			with open(pathToFile, 'rb') as f:
+				data = f.read()
+			decrypted = self.fernet.decrypt(data)
+			out = pathToFile.split(".{}".format(pathToFile.split(".")[-1]))[0]
+			with open(out, 'wb') as f:
+				f.write(decrypted)
+		except Exception as e:
+			return False
